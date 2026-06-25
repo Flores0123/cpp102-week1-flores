2bed9206a59935747b143e1711dca74746f5ba3e
Flores0123 <sedromeflores61@gmail.com>
2026-06-25 14:34:36 +0800

    Update hello_cpp102.py

--- hello_cpp102.py
+++ hello_cpp102.py
@@ -1,3 +1,3 @@
 print("Hello, CPP102!")
-print("My name is Juan Dela Cruz, Section 1CpE B.")
+print("My name is Adrian Sedrome Flores, Section 1CpE B.")
 print("I am learning Python, Git, and Github.")
